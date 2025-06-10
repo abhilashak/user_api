3df67cb96b6ada864073852ada9c34c53f84fff3
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-10 17:43:43 +0530

    enha: remove unwanted files and code #19

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,14 +38,8 @@ Rails.application.configure do
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
